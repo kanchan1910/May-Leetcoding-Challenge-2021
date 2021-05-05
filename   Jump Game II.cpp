e833639883827958ca5d37c67b9ae55260f3fb00
Kanchan Jeswani <kanchanjeswani2000@gmail.com>
2021-05-05 14:58:32 +0530

    Added solution for the problem in CPP

---
+++   Jump Game II.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int fun(vector<int>&nums, vector<int>& dp, int index)
+    {
+        // base case
+        if(index >= nums.size())
+        {
+            return nums.size();
+        }
+        if(index == nums.size() - 1)
+        {
+            return 0;
+        }
+        if(dp[index] != -1)
+        {
+            return dp[index];
+        }
+        int ans = nums.size();
+        for(int i = index + 1; i <= index + nums[index]; i++)
+        {
+            ans = min(ans, fun(nums, dp, i) + 1);
+        }
+        dp[index] = ans;
+        return dp[index];
+    }
+    int jump(vector<int>& nums)
+    {
+        int n = nums.size();
+        vector<int>dp(n, -1);
+        int index = 0;
+        return fun(nums, dp, index);
+        
+    }
+};

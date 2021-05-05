85ae3bc63a9ea7c57aae5eaf0cec20850edab782
Kanchan Jeswani <kanchanjeswani2000@gmail.com>
2021-05-05 15:30:29 +0530

    Added solution for the problem in CPP

---   Jump Game II.cpp
+++   Jump Game II.cpp
@@ -1,3 +1,6 @@
+//You can assume that you can always reach the last index.
+
+// so no worries to handle for that case
 class Solution {
 public:
     int fun(vector<int>&nums, vector<int>& dp, int index)
